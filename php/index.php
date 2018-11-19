5e4839f8cb588a44f86d6e3b51073b7a90b91bc4
Tobse <cthullu@protonmail.com>
2018-11-19 20:36:24 +0100

    Fixed DNS resolution and multiple instances issues of VPN script

---
+++ php/index.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <h1>Hello!</h1>
+    <p>
+      This site provides some helper scripts to retrieve https
+      traffic via WGET versions without http implementation.
+    </p>
+  </body>
+</html>
